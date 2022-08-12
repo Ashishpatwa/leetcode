f1c706ec52fcb152f0f120ebd572699a4deb3a70
Ashishpatwa <52313013+Ashishpatwa@users.noreply.github.com>
2022-08-12 13:43:22 +0530

    Time: 843 ms (53.59%), Space: 25.1 MB (78.68%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -4,15 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # i=0
-        # while nums[nums[i]]>0:
-        #     nums[nums[i]]*=-1
-        #     i=abs(nums[nums[i]])
-        # return abs(nums[i])
+        # # negative marking o(n) o(1) 
+        # for i in range(len(nums)):
+        #     v= abs(nums[i])
+        #     if nums[v]<0:
+        #         return v
+        #     nums[v]*=-1
         
-        for i in range(len(nums)):
-            v= abs(nums[i])
-            if nums[v]<0:
-                return v
-            nums[v]*=-1
+        # cycle detection o(n) o(1)
+        fast=nums[0]
+        slow=nums[0]
+        
+        while True:
+            fast= nums[nums[fast]]
+            slow= nums[slow]
+            if fast==slow:
+                break
+        
+        slow=nums[0]
+        while fast!=slow:
+            fast=nums[fast]
+            slow=nums[slow]
+        return fast
+        
         
