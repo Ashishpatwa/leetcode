f59f46029c717070d888fc05c8955e76a88a6ac2
Ashishpatwa <52313013+Ashishpatwa@users.noreply.github.com>
2022-09-04 11:31:00 +0530

    Time: 63 ms (59.14%), Space: 14.1 MB (94.35%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,30 +1,38 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
-        ans=set()
-        tmp=[]
+        ans=[]
         for i in range(1,10):
-            if i<k and k+i<=9:
-                tmp.append(str(i))
-                self.recursion(n,k,tmp,ans)
-            elif i>=k:
-                tmp.append(str(i))
-                self.recursion(n,k,tmp,ans)
-            tmp=[]
-        return list(ans)
+            self.recursion(n-1,k,i,ans)
+        return ans
+    
     def recursion(self,n,k,tmp,ans):
-        if len(tmp)>n:
+        if n==0:
+            ans.append(tmp)
             return 0
-        if len(tmp)==n:
-            ans.add(int("".join(tmp)))
-            return 0
-        if int(tmp[-1])+k<=9:
-            tmp.append(str(int(tmp[-1])+k))
-            self.recursion(n,k,tmp,ans)
-            tmp.pop()
-        if int(tmp[-1])-k>=0:
-            tmp.append(str(int(tmp[-1])-k))
-            self.recursion(n,k,tmp,ans)
-            tmp.pop()
+        var=tmp%10
+
+        if var+k<=9:
+            self.recursion(n-1,k,tmp*10+(var+k),ans)
+        if k>0 and var-k>=0:
+            self.recursion(n-1,k,tmp*10+(var-k),ans)
+    
+#     ans=[]
+#         tmp=""
+#         for i in range(1,10):
+#             self.recursion(n,k,tmp+str(i),ans)
+
+#         return ans
+#     def recursion(self,n,k,tmp,ans):
+#         if len(tmp)==n:
+#             print(int(tmp))
+#             ans.append(int(tmp))
+#             return 0
+#         flag=int(tmp[-1])+k
+#         if flag<=9:
+#             self.recursion(n,k,str(flag),ans)
+#         flag2=int(tmp[-1])-k
+#         if k>0 and flag>=0:
+#             self.recursion(n,k,str(flag2),ans)
             
         
         
