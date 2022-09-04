f95f989d37ab60ea522ef682903550e21049024a
Ashishpatwa <52313013+Ashishpatwa@users.noreply.github.com>
2022-09-04 11:50:34 +0530

    Time: 61 ms (64.12%), Space: 14 MB (94.35%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -2,37 +2,35 @@ class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
         ans=[]
         for i in range(1,10):
-            self.recursion(n-1,k,i,ans)
+            self.recursion(n,k,str(i),ans)
         return ans
-    
     def recursion(self,n,k,tmp,ans):
-        if n==0:
-            ans.append(tmp)
+        if len(tmp)==n:
+            ans.append(int(tmp))
             return 0
-        var=tmp%10
-
-        if var+k<=9:
-            self.recursion(n-1,k,tmp*10+(var+k),ans)
-        if k>0 and var-k>=0:
-            self.recursion(n-1,k,tmp*10+(var-k),ans)
-    
-#     ans=[]
-#         tmp=""
+        flag=int(tmp[-1])+k
+        if flag<=9:
+            self.recursion(n,k,tmp+str(flag),ans)
+        flag2=int(tmp[-1])-k
+        if k>0 and flag2>=0:
+            self.recursion(n,k,tmp+str(flag2),ans)
+#         ans=[]
 #         for i in range(1,10):
-#             self.recursion(n,k,tmp+str(i),ans)
-
+#             self.recursion(n-1,k,i,ans)
 #         return ans
+    
 #     def recursion(self,n,k,tmp,ans):
-#         if len(tmp)==n:
-#             print(int(tmp))
-#             ans.append(int(tmp))
+#         if n==0:
+#             ans.append(tmp)
 #             return 0
-#         flag=int(tmp[-1])+k
-#         if flag<=9:
-#             self.recursion(n,k,str(flag),ans)
-#         flag2=int(tmp[-1])-k
-#         if k>0 and flag>=0:
-#             self.recursion(n,k,str(flag2),ans)
+#         var=tmp%10
+
+#         if var+k<=9:
+#             self.recursion(n-1,k,tmp*10+(var+k),ans)
+#         if k>0 and var-k>=0:
+#             self.recursion(n-1,k,tmp*10+(var-k),ans)
+    
+    
             
         
         
