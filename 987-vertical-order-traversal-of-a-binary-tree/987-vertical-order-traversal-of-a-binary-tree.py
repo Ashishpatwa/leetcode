72669de0b551bdb3448656bfe1f680b6b047e63e
Ashishpatwa <52313013+Ashishpatwa@users.noreply.github.com>
2022-09-11 12:07:48 +0530

    Time: 37 ms (39.29%), Space: 13.9 MB (46.88%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def verticalTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        dic=dict()
+        self.bfs(root,dic,0)
+        ans=[]
+        k=dic.keys()
+        k.sort()
+        for i in k:
+            ans.append(dic[i])
+        return ans
+    def bfs(self,root,dic,d):
+        queue=deque()
+        queue.append([root,d])
+        while queue:
+            ln=len(queue)
+            tmp_dict=dict()
+            for i in range(ln):
+                rt,ind=queue.popleft()
+                if rt.left:
+                    queue.append([rt.left,ind-1])
+                if rt.right:
+                    queue.append([rt.right,ind+1])
+                if ind in tmp_dict:
+                    tmp_dict[ind].append(rt.val)
+                else:
+                    tmp_dict[ind]=[rt.val]
+            
+            for i in tmp_dict:
+                if i in dic:
+                    #print(tmp_dict[i])
+                    for j in sorted(tmp_dict[i]):
+                        dic[i].append(j)
+                else:
+                    dic[i]=sorted(tmp_dict[i])
+                
+                    
+        
