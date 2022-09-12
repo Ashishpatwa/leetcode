1093971105a48988b7de1b9ce568dde71e429001
Ashishpatwa <52313013+Ashishpatwa@users.noreply.github.com>
2022-09-12 14:17:21 +0530

    Time: 3808 ms (8.45%), Space: 14.3 MB (33.75%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        freq=dict()
+        for i in words:
+            freq[i]=freq.get(i,0)+1
+        c=len(words)*len(words[0])
+        ans=[]
+        i=0
+        while i<len(s)-c+1:
+            flag=1
+            freqs=copy.deepcopy(freq)
+            j=i
+            while j<i+c:
+                k=s[j:j+len(words[0])]
+                if k in freqs and freqs[k]:
+                    freqs[k]-=1
+                    j+=len(words[0])
+                else:
+                    flag=0
+                    break
+            if flag:
+                ans.append(i)
+            
+            i+=1
+        return ans
+            
+            
+        
